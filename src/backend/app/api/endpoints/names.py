abdc13b3d030e565957ebcfe62018fe839ae75a0
Andrew Neilan <ajn1390@gmail.com>
2025-04-05 22:10:17 -0400

    del

--- src/backend/app/api/endpoints/names.py
+++ src/backend/app/api/endpoints/names.py
@@ -19,48 +19,6 @@ router = APIRouter()
 
 init_db()
 
-# not sure if the response model should be read name or name base but ok for now
-
-
-@router.post("/names/", response_model=NameOut)
-async def create_your_name(name_in: NameCreate, db: Session = Depends(get_db)):
-    return create_name(db, name_in)
-
-
-@router.post("/names/", response_model=list[NameOut])
-async def read_your_names(
-    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
-    return admin_get_names(db, skip=skip, limit=limit)
-
-
-@router.post("/names/{name_id}", response_model=NameOut)
-async def read_your_name(name_id: int, db: Session = Depends(get_db)):
-    name = admin_get_name(db, name_id)
-    if not name:
-        raise HTTPException(status_code=404, detail="Name not found")
-    return name
-
-
-@router.put("/names/{name_id}", response_model=NameOut)
-async def update_your_name(
-    name_id: int, name_in: NameUpdate, db: Session = Depends(get_db)
-):
-    name = admin_get_name(db, name_id)
-    if not name:
-        raise HTTPException(status_code=404, detail="Name not found")
-    updated_name = update_name(db, name, name_in)
-    return updated_name
-
-
-@router.delete("/names/{name_id}", response_model=NameOut)
-async def delete_your_name(name_id: int, db: Session = Depends(get_db)):
-    name = admin_get_name(db, name_id)
-    if not name:
-        raise HTTPException(status_code=404, detail="Name not found")
-    deleted_name = admin_delete_name(db, name)
-    return deleted_name
-
 
 ##############
 
