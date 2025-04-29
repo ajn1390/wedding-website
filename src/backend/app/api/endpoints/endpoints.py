6f8f8009d7e3211194ad7d8e915287f43705a0e0
Andrew Neilan <ajn1390@gmail.com>
2025-04-29 18:43:54 -0400

    contact_info

--- src/backend/app/api/endpoints/endpoints.py
+++ src/backend/app/api/endpoints/endpoints.py
@@ -5,6 +5,9 @@ from src.backend.app.api.utils.storage import get_db
 from src.backend.app.db.crud.crud import (
     assign_secondary_guest_to_party,
     create_party_from_primary_guest,
+    delete_guest_info,
+    get_all_guest_data_by_id,
+    get_contact_info_by_id,
     get_guest_by_email,
     get_party_by_guest_id,
     guest_create,
@@ -12,6 +15,7 @@ from src.backend.app.db.crud.crud import (
 )
 from src.backend.app.db.database import init_db
 from src.backend.app.db.models.party import Party
+from src.backend.app.models.contact_info import ContactInfoBase
 from src.backend.app.models.guest import GuestCreate, GuestRead
 from src.backend.app.models.party import PartyRead
 
@@ -34,6 +38,32 @@ async def create_guest(guest_data: GuestCreate, db: Session = Depends(get_db)):
         create_party_from_primary_guest(db, new_guest)
 
     return new_guest
+
+
+@router.get("/guests/{guest_id}", response_model=GuestRead)
+async def read_guest_by_id(guest_id: int, db: Session = Depends(get_db)):
+    guest = get_all_guest_data_by_id(db, guest_id)
+    if guest is None:
+        raise HTTPException(status_code=404, detail=f"Guest {guest_id} not found")
+    return guest
+
+
+@router.get("/guests/by-email", response_model=GuestRead)
+async def admin_guest_by_email(email: str, db: Session = Depends(get_db)):
+    guest = get_guest_by_email(db, email)
+    if not guest:
+        raise HTTPException(
+            status_code=404, detail=f"No guest found with email {email}"
+        )
+    return guest
+
+
+@router.delete("guests/{guest_id}", response_model=GuestRead)
+async def delete_guest(guest_id: int, db: Session = Depends(get_db)):
+    guest = delete_guest_info(db, guest_id)
+    if guest is None:
+        raise HTTPException(status_code=404, detail=f"Guest {guest_id} not found")
+    return guest
 
 
 def party_guests(party: Party):
@@ -63,7 +93,7 @@ async def assign_secondary_guest(
     return party_guests(party)
 
 
-@router.get("/party/by-email", response_model=PartyRead)
+@router.get("/parties/by-email", response_model=PartyRead)
 async def get_party_guests_by_email(email: str, db: Session = Depends(get_db)):
     guest = get_guest_by_email(db, email)
     if not guest:
@@ -71,7 +101,18 @@ async def get_party_guests_by_email(email: str, db: Session = Depends(get_db)):
             status_code=404, detail=f"No guest found with email {email}"
         )
     party = get_party_by_guest_id(db, guest.id)
+    print("GUEST", guest.id, "PARTY", party)
     if not party:
         raise HTTPException(status_code=404, detail="No party found for guest")
 
     return party_guests(party)
+
+
+@router.get("/guests/{guest_id}/contact-info", response_model=ContactInfoBase)
+async def read_contact_by_id(guest_id: int, db: Session = Depends(get_db)):
+    contact_info = get_contact_info_by_id(db, guest_id)
+    if not contact_info:
+        raise HTTPException(
+            status_code=404, detail=f"No contact info found for guest {guest_id}"
+        )
+    return contact_info
